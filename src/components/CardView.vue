98a6087faa82bd23645471ae3c95fc7bbc509cd4
陳建佑 <jianyou224@gmail.com>
2022-08-28 15:44:24 +0800

    card

---
+++ src/components/CardView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col col-12 col-md-6 col-lg-4 my-2" v-for="(item, index) in data" :key="index">
+    <div class="card h-100">
+      <div class="card-header bg-secondary">
+        <p class="mb-0 text-white">
+          公廁名稱 :
+          <span class="text-warning"> {{ item.公廁名稱 }} </span>
+        </p>
+      </div>
+      <div class="card-body d-flex flex-column justify-content-between">
+        <h6 class="card-title mb-3">地址: {{ item.公廁地址 }} </h6>
+        <div class="d-flex align-items-center justify-content-between">
+          <p class="mb-0 text-danger d-inline-block pe-3 mb-2">
+            經度: {{ item.經度 }} <br>緯度: {{ item.緯度 }}
+          </p>
+          <a href="#" class="btn btn-outline-dark text-nowrap"
+          @click.prevent="goMap(item.緯度, item.經度)">地圖</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  data() {
+    return {
+    };
+  },
+  methods: {
+    goMap(w, j) {
+      console.log(w, j);
+      window.location.href = `https://www.google.com.tw/maps/place/${w},${j}`;
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,18 @@
 <template>
-  <div class="wrapper">
-    <header class="header">
-      <h1 class="text-center text-white bg-dark bg-opacity-75 py-2">
-        peeTaipei 台北尿尿網
-      </h1>
+  <header class="header">
+    <h1 class="text-center text-white bg-dark bg-opacity-50 py-3">
+      PeeTaipei 台北尿尿網
+    </h1>
 
-      <div class="container search-bar-wrap">
-        <input class="form-control" type="search" placeholder="輸入區或路名" aria-label="Search">
-      </div>
-    </header>
-    <section>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Libero esse temporibus est sed natus reprehenderit
-        rem, odio possimus, nesciunt, labore illum nihil. Corrupti
-        molestias fugit tenetur iure tempore doloremque eos.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Libero esse temporibus est sed natus reprehenderit
-        rem, odio possimus, nesciunt, labore illum nihil. Corrupti
-        molestias fugit tenetur iure tempore doloremque eos.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Libero esse temporibus est sed natus reprehenderit
-        rem, odio possimus, nesciunt, labore illum nihil. Corrupti
-        molestias fugit tenetur iure tempore doloremque eos.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Libero esse temporibus est sed natus reprehenderit
-        rem, odio possimus, nesciunt, labore illum nihil. Corrupti
-        molestias fugit tenetur iure tempore doloremque eos.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Libero esse temporibus est sed natus reprehenderit
-        rem, odio possimus, nesciunt, labore illum nihil. Corrupti
-        molestias fugit tenetur iure tempore doloremque eos.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Libero esse temporibus est sed natus reprehenderit
-        rem, odio possimus, nesciunt, labore illum nihil. Corrupti
-        molestias fugit tenetur iure tempore doloremque eos.
-      </p>
+    <div class="container search-bar-wrap">
+      <input class="form-control" type="search" placeholder="請輸入區或路名尋找附近的廁所" aria-label="Search">
+    </div>
+  </header>
+  <div class="wrapper">
+    <section class="container">
+      <CardView
+      :data = data>
+      </CardView>
     </section>
     <footer class="footer bg-dark text-secondary text-center py-3 mt-auto">
       PeeTaipei 關心您尿尿的地方<br><span class="fz-14">Copyright &copy;2022 Chen ChienYu</span>
@@ -53,31 +20,69 @@
   </div>
 </template>
 
+<script>
+import CardView from '../components/CardView.vue';
+
+export default {
+  components: {
+    CardView,
+  },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  methods: {
+    getData() {
+      const url = 'data/64e392a0fcb63fa9fb1f596bcb86e96b_export.json';
+      this.axios.get(url)
+        .then((res) => {
+          this.data = res.data;
+          console.log(this.data);
+        });
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+
+</script>
+
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400&display=swap');
-.wrapper{
-  height: 100%;
-  border: 1px solid #aaa;
-  display: flex;
-  flex-direction: column;
-}
+
 .header {
   position: relative;
   height: 260px;
   background-image: url('../assets/img/pexels-cottonbro-4114710.jpg');
-  background-position: 50% 32%;
+  background-position: 50% 33%;
   background-size: cover;
 
   h1 {
     font-family: 'Noto Sans TC', sans-serif;
-    font-size: 18px;
+    font-size: 16px;
+    letter-spacing: .1rem;
+  }
+
+  .search-bar-wrap {
+    position: absolute;
+    bottom: 35%;
+    transform: translateY(50%);
+    // border: 1px solid #f00;
+    display: flex;
+    justify-content: center;
+
+    input {
+      width: 75%;
+    }
   }
 }
 
-.search-bar-wrap {
-  position: absolute;
-  bottom: 23%;
-  right: 0;
-  left: 0;
+.wrapper {
+  height: calc(100vh - 260px);
+  // border: 1px solid #f00;
+  display: flex;
+  flex-direction: column;
 }
 </style>
