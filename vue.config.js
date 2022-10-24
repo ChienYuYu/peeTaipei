5d8115e9c09ee080cf2dff3badf309fe168ec08b
陳建佑 <jianyou224@gmail.com>
2022-10-24 10:25:37 +0800

    使用配置代理

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,14 @@ module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/peeTaipei/'
     : '/',
+
+  devServer: {
+    proxy: {
+      '^/toilet': {
+        target: 'https://quality.data.gov.tw/dq_download_json.php?nid=138798&md5_url=64e392a0fcb63fa9fb1f596bcb86e96b',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
 });
